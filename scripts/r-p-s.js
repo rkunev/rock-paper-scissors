3da0af91104c3f9ce2cd6137d8fc8100f1aa1005
Rosen <rkunev@gmail.com>
2013-06-28 17:52:53 +0300

    JS completed, minor changes to HTML and CSS
    HTML:
    -  Added to span elements to display user and computer choices
    CSS:
    -  Minor margin and padding changes to info elements
    JS:
    -  Added hover animation to game items
    -  Completed game logic
    
    BUGS:
    -  Game items are not horizontally centered

--- scripts/r-p-s.js
+++ scripts/r-p-s.js
@@ -1,25 +1,73 @@
 $(document).ready(function() {
-    var state = true;
-    $("img").click(function() {
-        if (state) {
-            // resets all img changes
-            $("img").css("width", "175");
-            $("img").removeClass("userChoice");
-
-            // animates clicked img
-            $(this).animate({ 
-              width: 200
-            }, 100);
-            $(this).toggleClass("userChoice");
+    // hover animation
+    $(".items img").hover(
+        function() {
+            $(this).animate({ width: 200 }, 100);
+        },
+        function() {
+            $(this).animate({ width: 175 }, 100);
         }
+    );
+
+    // game logic
+    $(".items img").click(function() {
+
+        var computerChoice = Math.round(Math.random() * 3),
+            userChoice = $(this).attr("alt"),
+            result = $("#result"),
+            userTextField = $("#user-field"),
+            computerTextField = $("#computer-field");
+
+        userTextField.html("You chose " + userChoice);
+
+        // computer's choice is rock
+        if (computerChoice === 0) {
+            computerChoice = "rock";
+            computerTextField.html("Computer chose " + computerChoice);
+
+            if (userChoice === "rock") {
+                result.html("Tie!");
+            }
+            else if (userChoice === "paper") {
+                result.html("You win");
+            }
+            // if userChoice is scissors
+            else {
+                result.html("Computer wins");
+            }
+        }
+        // computer's choice is paper
+        else if (computerChoice === 1) {
+            computerChoice = "paper";
+            computerTextField.html("Computer chose " + computerChoice);
+
+            if (userChoice === "rock") {
+                result.html("Computer wins");
+            }
+            else if (userChoice === "paper") {
+                result.html("Tie!");
+            }
+            // if userChoice is scissors
+            else {
+                result.html("You win");
+            }
+        }
+        // computer's choice is scissors
         else {
-            $(this).animate({
-              width: 175
-            }, 100);
+            computerChoice = "scissors";
+            computerTextField.html("Computer chose " + computerChoice);
 
-            $(this).toggleClass("userChoice");
+            if (userChoice === "rock") {
+                result.html("You win");
+            }
+            else if (userChoice === "paper") {
+                result.html("Computer wins");
+            }
+            // if userChoice is scissors
+            else {
+                result.html("Tie!");
+            }
         }
-        state = !state;
     });
 });
 
